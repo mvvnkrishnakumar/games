d47543fac52f7120ad6b4cf28811a56b55997ac7
Niharikakasthuri <kasthuriniharika32@gmail.com>
2024-06-10 12:38:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {Routes, Route} from 'react-router-dom'
 
-export default App;
+import GamigHomeScreen from './components/GamingHomeScreen'
+import RPSgameHomePage from './components/RPSgameHomePage'
+import RPSgame from './components/RPSgame'
+import EmojiRules from './components/EmojiRules'
+import EmojiGame from './components/EmojiGame'
+import EmojiGameResult from './components/EmojiGameResultView'
+import CardFlipHomePage from './components/CardFlipHomePage'
+import CardFlipGame from './components/CardFlipGame'
+import MemoryMatrixHome from './components/MemoryMatrixHome'
+import MemoryGameBoard from './components/MemoryMatrixGameBoard'
+
+const App = () => (
+  <Routes>
+    <Route exact path="/" element={<GamigHomeScreen/>} />
+    <Route exact path="/rpsGame" element={<RPSgameHomePage/>} />
+    <Route exact path="/playRpsgame" element={<RPSgame/>} />
+    <Route exact path="/emojiGame" element={<EmojiRules/>} />
+    <Route exact path="/playEmojiGame" element={<EmojiGame/>} />
+    <Route exact path="/emojiResult" element={<EmojiGameResult/>} />
+    <Route exact path="/cardFlipGame" element={<CardFlipHomePage/>} />
+    <Route exact path="/playCardflipGame" element={<CardFlipGame/>} />
+    <Route exact path="/memoryMatrixGame" element={<MemoryMatrixHome/>} />
+    <Route exact path="/playMemoryMatrixgame" element={<MemoryGameBoard/>} />
+  </Routes>
+)
+
+export default App
